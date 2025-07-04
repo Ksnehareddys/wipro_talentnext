d8056b0399ee46167c9e74d3d4f889247df5f33a
Snehalatha Reddy <snehalathareddy91771@gmail.com>
2025-07-04 15:34:54 +0530

    java

---
+++ java_fundamentals/src/java_fundamentals/Question1.java
@@ -0,0 +1,14 @@
+package java_fundamentals;
+
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+         String s=sc.nextLine();
+	     String s1=sc.nextLine();
+         System.out.println(s+" Tehnologies "+s1);
+	}
+
+}
